f35b3dab90f281231f487447d16e5c156de099d5
turutupa <adc.batros@gmail.com>
2021-10-26 13:36:31 -0700

    added comments

--- src/main/java/com/cs601/project3/server/models/HttpHeader.java
+++ src/main/java/com/cs601/project3/server/models/HttpHeader.java
@@ -1,14 +1,17 @@
 package com.cs601.project3.server.models;
 
 public class HttpHeader {
+    // protocol version
     private static final String LINE_END = "\n\r\n";
     public static final String VERSION = "HTTP/1.1";
 
+    // status codes
     public static final String OK = headerify("200 OK");
     public static final String BAD_REQUEST = headerify("400 Bad Request");
     public static final String NOT_FOUND = headerify("404 Not Found");
     public static final String NOT_ALLOWED = headerify("405 Method Not Allowed");
 
+    // optional headers
     public static final String CONTENT_LENGTH = "Content-Length:";
     public static final String CONNECTION_CLOSE = "Connection: close";
 
