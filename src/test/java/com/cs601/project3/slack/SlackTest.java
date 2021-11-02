b1766f86471e861a7a120da64de578aab25fb0b4
turutupa <adc.batros@gmail.com>
2021-11-01 18:00:08 -0700

    refactored tests

--- src/test/java/com/cs601/project3/slack/SlackTest.java
+++ src/test/java/com/cs601/project3/slack/SlackTest.java
@@ -4,10 +4,7 @@ import com.cs601.project3.HtmlValidator;
 import com.cs601.project3.clientRequest.ClientRequest;
 import com.cs601.project3.clientRequest.ClientResponse;
 import com.cs601.project3.server.models.HttpStatus;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 import java.net.ConnectException;
@@ -20,21 +17,30 @@ class SlackTest {
     String SHUTDOWN_PATH = "/shutdown";
     String URL = DOMAIN + SHUTDOWN_PATH;
     public static String[] args;
+    Thread serverThread;
 
     @BeforeAll
     public static void setUp() {
         args = new String[0];
     }
 
+    @BeforeEach
+    public void setUpBeforeEach() {
+        serverThread = new Thread(() -> Slack.main(args));
+        serverThread.start();
+    }
+
+    @AfterEach
+    public void cleanUp() throws InterruptedException {
+        Slack.shutdown();
+        serverThread.join();
+    }
+
     @Test
     @DisplayName("should shutdown server on Get request to /shutdown endpoint")
     void throwsOnServerShutdown() throws IOException, InterruptedException {
-        Thread serverThread = new Thread(() -> Slack.main(args));
-        serverThread.start();
-
         TimeUnit.SECONDS.sleep(1);
         ClientRequest.get(URL);
-        serverThread.join();
 
         Assertions.assertThrows(ConnectException.class, () -> ClientRequest.get(URL));
     }
@@ -42,12 +48,8 @@ class SlackTest {
     @Test
     @DisplayName("should return correct headers/body on shutdown call")
     void headers() throws IOException, InterruptedException {
-        Thread serverThread = new Thread(() -> Slack.main(args));
-        serverThread.start();
-
         TimeUnit.SECONDS.sleep(1);
         ClientResponse res = ClientRequest.get(URL);
-        serverThread.join();
 
         Assertions.assertEquals(200, res.statusCode);
         Assertions.assertTrue(HtmlValidator.isValid(res.body));
--- src/test/java/com/cs601/project3/slack/api/bot/PublishMessageTest.java
+++ src/test/java/com/cs601/project3/slack/api/bot/PublishMessageTest.java
@@ -18,17 +18,22 @@ class PublishMessageTest {
     private static final int PORT = 8000;
     final String PATH = "/slackbot";
     final String URL = "http://localhost:" + PORT + PATH;
+    Thread serverThread;
 
     @BeforeEach
     void setUp() {
         app = new Server(PORT);
         app.get(PATH, Bot.messageForm);
         app.post(PATH, Bot.publishMessage);
+
+        serverThread = new Thread(app);
+        serverThread.start();
     }
 
     @AfterEach
-    void clean() {
+    void clean() throws InterruptedException {
         app.shutdown();
+        serverThread.join();
     }
 
     @Test
@@ -39,106 +44,58 @@ class PublishMessageTest {
 
     @Test
     @DisplayName("should have correct headers")
-    void hasCorrectHeaders() throws InterruptedException {
-        Thread serverThread = new Thread(app);
-        Thread clientThread = new Thread(() -> {
-            ClientResponse res = null;
-            try {
-                res = ClientRequest.get(URL);
-                Assertions.assertEquals(200, res.statusCode);
-                Assertions.assertEquals(HttpStatus.VERSION, res.protocol);
-                Assertions.assertEquals("GET", res.method);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        // start threads
-        serverThread.start();
-        clientThread.start();
-
-        // shut everything down
-        clientThread.join();
-        app.shutdown();
-        serverThread.join();
+    void hasCorrectHeaders() {
+        ClientResponse res = null;
+        try {
+            res = ClientRequest.get(URL);
+            Assertions.assertEquals(200, res.statusCode);
+            Assertions.assertEquals(HttpStatus.VERSION, res.protocol);
+            Assertions.assertEquals("GET", res.method);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     @DisplayName("should have correct xhtml body")
-    void hasCorrectBody() throws InterruptedException {
-        Thread serverThread = new Thread(app);
-        Thread clientThread = new Thread(() -> {
-            ClientResponse res = null;
-            try {
-                res = ClientRequest.post(URL, "message=test+message");
-                Assertions.assertEquals(200, res.statusCode);
-                Assertions.assertTrue(HtmlValidator.isValid(res.body));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        // start threads
-        serverThread.start();
-        clientThread.start();
-
-        // shut everything down
-        clientThread.join();
-        app.shutdown();
-        serverThread.join();
+    void hasCorrectBody() {
+        ClientResponse res = null;
+        try {
+            res = ClientRequest.post(URL, "message=test+message");
+            Assertions.assertEquals(200, res.statusCode);
+            Assertions.assertTrue(HtmlValidator.isValid(res.body));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     @DisplayName("should return bad request because bad form message submission")
-    void badRequest() throws InterruptedException {
-        Thread serverThread = new Thread(app);
-        Thread clientThread = new Thread(() -> {
-            ClientResponse res = null;
-            try {
-                res = ClientRequest.post(URL, "WRONG=test+message");
-                Assertions.assertEquals(400, res.statusCode);
-                Assertions.assertEquals(HttpStatus.VERSION, res.protocol);
-                Assertions.assertEquals("POST", res.method);
-                Assertions.assertTrue(HtmlValidator.isValid(res.body));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        // start threads
-        serverThread.start();
-        clientThread.start();
-
-        // shut everything down
-        clientThread.join();
-        app.shutdown();
-        serverThread.join();
+    void badRequest() {
+        ClientResponse res = null;
+        try {
+            res = ClientRequest.post(URL, "WRONG=test+message");
+            Assertions.assertEquals(400, res.statusCode);
+            Assertions.assertEquals(HttpStatus.VERSION, res.protocol);
+            Assertions.assertEquals("POST", res.method);
+            Assertions.assertTrue(HtmlValidator.isValid(res.body));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
     @Test
     @DisplayName("should have correct xhtml body the 404 NOT FOUND response")
-    void hasCorrectBodyBadRequestResponse() throws InterruptedException {
-        Thread serverThread = new Thread(app);
-        Thread clientThread = new Thread(() -> {
-            ClientResponse res = null;
-            try {
-                res = ClientRequest.get(URL + "/hello");
-                Assertions.assertEquals(404, res.statusCode);
-                Assertions.assertTrue(HtmlValidator.isValid(res.body));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        // start threads
-        serverThread.start();
-        clientThread.start();
-
-        // shut everything down
-        clientThread.join();
-        app.shutdown();
-        serverThread.join();
+    void hasCorrectBodyBadRequestResponse() {
+        ClientResponse res = null;
+        try {
+            res = ClientRequest.get(URL + "/hello");
+            Assertions.assertEquals(404, res.statusCode);
+            Assertions.assertTrue(HtmlValidator.isValid(res.body));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
